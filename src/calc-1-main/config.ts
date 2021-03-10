20c5b4d0cd1e549d1746add3bf56bc0af2f1c7f6
kirillpuzanov <kirillpuzanov@mail.ru>
2021-03-11 01:11:57 +0300

    success!!!!15

--- src/calc-1-main/config.ts
+++ src/calc-1-main/config.ts
@@ -1,11 +1,8 @@
 import dotenv from 'dotenv';
 
 // dotenv.config()
-const DB_password = process.env.DB_PASSWORD || 'uyhixqhHNVl3FNfk';
-const DB_userName = process.env.DB_USER_NAME || 'kirill';
 
-export const MongoDBUris = `mongodb+srv://${DB_userName}:${DB_password}@cluster0.hxs1t.mongodb.net/calcTest?retryWrites=true&w=majority`;
-
+export const MongoDBUris = `mongodb+srv://kirill:uyhixqhHNVl3FNfk@cluster0.hxs1t.mongodb.net/calcTest?retryWrites=true&w=majority`;
 
 export const _PORT = 3007;
 
