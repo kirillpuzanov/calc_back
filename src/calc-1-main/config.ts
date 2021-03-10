51d323925f3e4d2f699a3d7543a44ee92facb1b8
kirillpuzanov <kirillpuzanov@mail.ru>
2021-03-11 01:00:34 +0300

    success!!!!13

--- src/calc-1-main/config.ts
+++ src/calc-1-main/config.ts
@@ -6,5 +6,7 @@ const DB_userName = process.env.DB_USER_NAME
 
 export const MongoDBUris = `mongodb+srv://${DB_userName}:${DB_password}@cluster0.hxs1t.mongodb.net/calcTest?retryWrites=true&w=majority`;
 
+
+
 export const _PORT = 3007;
 
