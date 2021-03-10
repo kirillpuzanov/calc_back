50f9bdcf529d585eb017026cc4319c257a1a4974
kirillpuzanov <kirillpuzanov@mail.ru>
2021-03-11 00:50:16 +0300

    success!!!!

--- dist/calc-1-main/config.js
+++ dist/calc-1-main/config.js
@@ -8,6 +8,6 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const DB_password = process.env.DB_PASSWORD;
 const DB_userName = process.env.DB_USER_NAME;
-exports.MongoDBUris = `mongodb+srv://kirill:uyhixqhHNVl3FNfk@cluster0.hxs1t.mongodb.net/calcTest?retryWrites=true&w=majority`;
+exports.MongoDBUris = `mongodb+srv://${DB_userName}:${DB_password}@cluster0.hxs1t.mongodb.net/calcTest?retryWrites=true&w=majority`;
 exports._PORT = 3007;
 //# sourceMappingURL=config.js.map
